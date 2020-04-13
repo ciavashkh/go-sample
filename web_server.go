4aed2730b020282ade0b8a712776526c5ae7ba57
Siavash Khalvati <siavash.khalvati@mydrivesolutions.com>
2020-04-13 19:13:00 +0100

    initialize

---
+++ web_server.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "log"
+
+    "html/template"
+    "net/http"
+)
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/view/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+        return
+    }
+    renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/edit/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/save/"):]
+    body := r.FormValue("body")
+    p := &Page{Title: title, Body: []byte(body)}
+    p.save()
+    http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/upload/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    renderTemplate(w, "upload", p)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+    t, _ := template.ParseFiles(tmpl + ".html")
+    t.Execute(w, p)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+    http.HandleFunc("/view/", viewHandler)
+    http.HandleFunc("/edit/", editHandler)
+    http.HandleFunc("/save/", saveHandler)
+    http.HandleFunc("/upload/", uploadHandler)
+    http.HandleFunc("/uploadfile/", uploadFile)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+type Page struct {
+    Title string
+    Body  []byte
+}
+
+func (p *Page) save() error {
+    filename := p.Title + ".txt"
+    return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+    filename := title + ".txt"
+    body, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+    return &Page{Title: title, Body: body}, nil
+}
+
+//func main() {
+//    p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+//    p1.save()
+//    p2, _ := loadPage("TestPage")
+//    fmt.Println(string(p2.Body))
+//}
+
+
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("File Upload Endpoint Hit")
+
+    // Parse our multipart form, 10 << 20 specifies a maximum
+    // upload of 10 MB files.
+    r.ParseMultipartForm(10 << 20)
+    // FormFile returns the first file for the given key `myFile`
+    // it also returns the FileHeader so we can get the Filename,
+    // the Header and the size of the file
+    file, handler, err := r.FormFile("myFile")
+    if err != nil {
+        fmt.Println("Error Retrieving the File")
+        fmt.Println(err)
+        return
+    }
+    defer file.Close()
+    fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+    fmt.Printf("File Size: %+v\n", handler.Size)
+    fmt.Printf("MIME Header: %+v\n", handler.Header)
+
+    // Create a temporary file within our temp-images directory that follows
+    // a particular naming pattern
+    //tempFile, err := ioutil.TempFile("temp-images", handler.Filename)
+    //fmt.Println(tempFile)
+    if err != nil {
+        fmt.Println(err)
+    }
+    //defer tempFile.Close()
+
+    // read all of the contents of our uploaded file into a
+    // byte array
+    fileBytes, err := ioutil.ReadAll(file)
+    if err != nil {
+        fmt.Println(err)
+    }
+    // write this byte array to our temporary file
+    //tempFile.Write(fileBytes)
+    file_name := fmt.Sprint("temp-images/", handler.Filename)
+    ioutil.WriteFile(file_name, fileBytes, 0644)
+    // return that we have successfully uploaded our file!
+    fmt.Fprintf(w, "Successfully Uploaded File\n")
+}
+
+
